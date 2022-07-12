42f9e5885820381b0fe69a24a7ee5ec58057bedf
mostaphaRoudsari <sadeghipour@gmail.com>
2022-07-12 05:01:12 -0400

    feat(writer): add support for shade groups

--- honeybee_ies/_extend_honeybee.py
+++ honeybee_ies/_extend_honeybee.py
@@ -1,6 +1,6 @@
-from .writer import shade_to_ies, room_to_ies, model_to_ies
+from .writer import _shade_to_ies, room_to_ies, model_to_ies
 from honeybee.model import Shade, Room, Model
 
-Shade.to_gem = shade_to_ies
+Shade.to_gem = _shade_to_ies
 Room.to_gem = room_to_ies
 Model.to_gem = model_to_ies
