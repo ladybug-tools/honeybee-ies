6986a10c7eefa8e3f30a831b875ba41530b14ed3
mostaphaRoudsari <sadeghipour@gmail.com>
2022-07-12 16:59:16 -0400

    fix(holes): add support for importing and exporting holes
    The current implementation, translates every hole to an AirBoundary inside the model.

--- tests/writer_test.py
+++ tests/writer_test.py
@@ -20,3 +20,19 @@ def test_room_shades():
     outf = model_to_ies(model, out_folder.as_posix(), name='lab_building')
     assert outf.exists()
     assert 'TYPE\n4' in outf.read_text()
+
+
+def test_air_boundary():
+    in_file = './tests/assets/room_with_air_boundary.hbjson'
+    out_folder = pathlib.Path('./tests/assets/temp')
+    out_folder.mkdir(parents=True, exist_ok=True) 
+    model = Model.from_hbjson(in_file)
+    outf = model_to_ies(model, out_folder.as_posix(), name='room_with_air_boundary')
+    assert outf.exists()
+    ab_str = '4 2\n' \
+        '   0.000000    0.000000\n' \
+        '   11.000000    0.000000\n' \
+        '   11.000000    3.000000\n' \
+        '   0.000000    3.000000\n'
+
+    assert ab_str in outf.read_text()
