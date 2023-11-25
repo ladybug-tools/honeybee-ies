e73b0faf34caaa1133b23b60d69356891addb69a
mostaphaRoudsari <sadeghipour@gmail.com>
2023-11-24 22:27:31 -0500

    feat(reader): add support for topographical shade
    See here: https://discourse.pollination.cloud/t/topographical-shade-export-from-gem-file/3295

--- tests/reader_test.py
+++ tests/reader_test.py
@@ -6,3 +6,9 @@ def test_underground_aperture():
     in_file = './tests/assets/room_underground.gem'
     model = model_from_ies(in_file)
     assert len(model.rooms) == 2
+
+
+def test_topo_shade():
+    in_file = './tests/assets/topographical_shade.gem'
+    model = model_from_ies(in_file)
+    assert len(model.shades) == 6
