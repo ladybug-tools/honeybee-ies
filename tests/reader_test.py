3593e0ebc2e6135559f6749aadf1264e38c3c845
mostaphaRoudsari <sadeghipour@gmail.com>
2023-12-01 15:48:14 -0500

    feat(reader): add support for translucent shade

--- tests/reader_test.py
+++ tests/reader_test.py
@@ -1,6 +1,5 @@
-import pathlib
-from honeybee.model import Model
 from honeybee_ies.reader import model_from_ies
+
 
 def test_underground_aperture():
     in_file = './tests/assets/room_underground.gem'
@@ -12,3 +11,9 @@ def test_topo_shade():
     in_file = './tests/assets/topographical_shade.gem'
     model = model_from_ies(in_file)
     assert len(model.shades) == 6
+
+
+def test_translucent_shade():
+    in_file = './tests/assets/translucent_shade.gem'
+    model = model_from_ies(in_file)
+    assert len(model.shades) == 1
