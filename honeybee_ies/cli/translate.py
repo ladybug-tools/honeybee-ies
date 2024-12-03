4ad99745afa9b7e10ee9eb8a7f63a11a6aa150fe
Chris Mackey <chris@ladybug.tools>
2024-12-03 14:34:16 -0800

    feat(properties): Add ModelIESProperties with a specific check for IES
    This change also refactors the CLI to be call-able as standalone functions

--- honeybee_ies/cli/translate.py
+++ honeybee_ies/cli/translate.py
@@ -1,13 +1,12 @@
 """honeybee ies translation commands."""
 import click
 import sys
-import pathlib
+import os
 import logging
 import json
 
-from ladybug.futil import write_to_file_by_name
 from honeybee.model import Model
-from honeybee_ies.writer import model_to_gem
+from honeybee_ies.writer import model_to_gem as model_to_gem_str
 from honeybee_ies.reader import model_from_ies
 
 _logger = logging.getLogger(__name__)
@@ -37,7 +36,7 @@ def translate():
     '--output-file', '-o', help='Optional GEM file path to output the GEM string '
     'of the translation. By default this will be printed out to stdout.',
     type=click.File('w'), default='-', show_default=True)
-def model_to_gem_file(model_file, shade_thickness, name, folder, output_file):
+def model_to_gem_cli(model_file, shade_thickness, name, folder, output_file):
     """Translate a Model JSON file to an IES GEM file.
     \b
 
@@ -45,19 +44,33 @@ def model_to_gem_file(model_file, shade_thickness, name, folder, output_file):
         model_file: Full path to a Model JSON file (HBJSON) or a Model pkl (HBpkl) file.
     """
     try:
-        model = Model.from_file(model_file)
-        gem_str = model_to_gem(model, shade_thickness=shade_thickness)
         if folder is not None and name is not None:
             if not name.lower().endswith('.gem'):
                 name = name + '.gem'
-            write_to_file_by_name(folder, name, gem_str, True)
-        else:
-            output_file.write(gem_str)
+            output_file = os.path.join(folder, name)
+        model_to_gem(model_file, shade_thickness, output_file)
     except Exception as e:
         _logger.exception('Model translation failed.\n{}'.format(e))
         sys.exit(1)
     else:
         sys.exit(0)
+
+
+def model_to_gem(model_file, shade_thickness=0, output_file=None):
+    """Translate a Model JSON file to an IES GEM file.
+
+    Args:
+        model_file: Full path to a Honeybee Model file (HBJSON or HBpkl).
+        shade_thickness: Optional value for shade thickness in meters. This value
+            will be used to extrude shades with no group id. IES does not consider
+            the effect of shades with no thickness in SunCalc. This function
+            extrudes the geometry to create a closed volume for the shade. (Default: 0).
+        output_file: Optional GEM file path to output the GEM string of the
+            translation. If None, the string will be returned from this function.
+    """
+    model = Model.from_file(model_file)
+    gem_str = model_to_gem_str(model, shade_thickness=shade_thickness)
+    return _process_content_to_output(gem_str, output_file)
 
 
 @translate.command('gem-to-model')
@@ -73,7 +86,7 @@ def model_to_gem_file(model_file, shade_thickness, name, folder, output_file):
     '--output-file', '-o', help='Optional HBJSON file path to output the HBJSON string '
     'of the translation. By default this will be printed out to stdout.',
     type=click.File('w'), default='-', show_default=True)
-def gem_to_model_file(gem_file, name, folder, output_file):
+def gem_to_model_cli(gem_file, name, folder, output_file):
     """Translate an IES GEM file to a HBJSON model.
     \b
 
@@ -81,15 +94,43 @@ def gem_to_model_file(gem_file, name, folder, output_file):
         gem-file: Full path to an IES VE GEM file.
     """
     try:
-        model = model_from_ies(gem_file)
         if folder is not None and name is not None:
-            folder = pathlib.Path(folder)
-            folder.mkdir(parents=True, exist_ok=True)
-            model.to_hbjson(name=name, folder=folder.as_posix())
-        else:
-            output_file.write(json.dumps(model.to_dict()))
+            low_name = name.lower()
+            if not low_name.endswith('.hbjson') and not low_name.endswith('.json'):
+                name = name + '.hbjson'
+            output_file = os.path.join(folder, name)
+        gem_to_model(gem_file, output_file)
     except Exception as e:
         _logger.exception('Model translation failed.\n{}'.format(e))
         sys.exit(1)
     else:
         sys.exit(0)
+
+
+def gem_to_model(gem_file, output_file=None):
+    """Translate an IES GEM file to a HBJSON model.
+
+    Args:
+        gem_file: Full path to an IES VE GEM file.
+        output_file: Optional HBJSON file path to output the JSON string of the
+            translation. If None, the string will be returned from this function.
+    """
+    model = model_from_ies(gem_file)
+    content_str = json.dumps(model.to_dict())
+    return _process_content_to_output(content_str, output_file)
+
+
+def _process_content_to_output(content_str, output_file):
+    """Process content strings for various types of output_files."""
+    if output_file is None:
+        return content_str
+    elif isinstance(output_file, str):
+        if not os.path.isdir(os.path.dirname(output_file)):
+            os.makedirs(os.path.dirname(output_file))
+        with open(output_file, 'w') as of:
+            of.write(content_str)
+    else:
+        if 'stdout' not in str(output_file):
+            if not os.path.isdir(os.path.dirname(output_file.name)):
+                os.makedirs(os.path.dirname(output_file.name))
+        output_file.write(content_str)
--- tests/extend_test.py
+++ tests/extend_test.py
@@ -1,11 +1,11 @@
-import pathlib
 from honeybee.model import Model
+from honeybee_ies.writer import model_to_gem
 
 
 def test_model():
+    # serialize the model
     in_file = './tests/assets/sample_model_45.hbjson'
-    out_folder = pathlib.Path('./tests/assets/temp')
-    out_folder.mkdir(parents=True, exist_ok=True) 
     model = Model.from_hbjson(in_file)
-    outf = model.to_gem(out_folder.as_posix(), name='sample_model_45')
-    assert outf.exists()
+    # check that the model can be translated to GEM
+    gem_str = model_to_gem(model)
+    assert isinstance(gem_str, str)
